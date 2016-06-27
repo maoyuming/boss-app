3f2f76dc354e66787d0335cb1a17709aa6ca7e10
lixuenan <xuenan.li@fangbaba.com>
2016-06-27 19:46:59 +0800

    增加service

---
+++ www/js/BossHotelBalanceService.js
@@ -0,0 +1,55 @@
+/**
+ * 点赞服务
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('starter.bossHotelBalanceService')
+    .service('BossHotelBalanceService', BossHotelBalanceService);
+
+  LikeService.$inject = ['DtkHttp'];
+
+  function BossHotelBalanceService(DtkHttp) {
+    return {
+      /**********************结算接口************************/
+      /**
+       * 查询账户余额
+       * @param params http header: token:Zd8H+oYcB1bGJtkxaSOjpnZ03+roT+MGMBq4dC56w0Y=
+                       Long bossId 老板id
+                       Long hotelId 酒店id
+       * @returns {*}
+       */
+      bossScBalance    : function (params) {
+        var act = '/boss/sc/balance';
+        return DtkHttp.post(act, params);
+      },
+      /**
+       * 发起提现申请
+       * @param params http header: token:Zd8H+oYcB1bGJtkxaSOjpnZ03+roT+MGMBq4dC56w0Y=
+                       Long bossId 老板id
+                       Long hotelId 酒店id
+                       BigDecimal sum 提现金额
+       * @returns {*}
+       */
+      bossScWithdrawl  : function (params) {
+        var act = '/boss/sc/withdrawl';
+        return DtkHttp.post(act, params);
+      },
+      /**
+       * 查询提现申请进度
+       * @param params http header: token:Zd8H+oYcB1bGJtkxaSOjpnZ03+roT+MGMBq4dC56w0Y=
+                        Long bossId 老板id
+                        Long hotelId 酒店id
+       * @returns {*}
+       */
+      bossScWithdrawlStatus   : function (params) {
+        var act = '/boss/sc/withdrawl/status';
+        return DtkHttp.post(act, params);
+      }
+    };
+
+  }
+})();
+
+
---
+++ www/js/MessageService.js
@@ -0,0 +1,58 @@
+/**
+ * 点赞服务
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('dtk.services')
+    .service('MessageService', MessageService);
+
+  UserService.$inject = ['DtkHttp'];
+
+  function MessageService(DtkHttp) {
+    return {
+
+      /**********************消息发送验证接口************************/
+      /**
+       * 发送短信
+       * @param params message:短信内容 phone:手机号码
+       * @returns {*}
+       */
+      messageSend : function (params) {
+        var act = '/message/send';
+        return DtkHttp.post(act, params);
+      },
+
+      /**
+       * 发送短信验证码
+       * @param params phone:手机号码
+       * @returns {*}
+       */
+      messageVerifyCodeSend  : function (params) {
+        var act = '/message/verifycode/send';
+        return DtkHttp.post(act, params);
+      },
+      /**
+       * 验证短信接口
+       * @param params phone:手机号码  verifycode:验证码
+       * @returns {*}
+       */
+      messageVerifyCodeVerify  : function (params) {
+        var act = '/message/verifycode/verify';
+        return DtkHttp.post(act, params);
+      },
+      /**
+       * 登录接口
+       * @param params phone:手机号码  verifycode:验证码
+       * @returns {*}
+       */
+      messageLogin  : function (params) {
+        var act = '/message/login';
+        return DtkHttp.post(act, params);
+      }
+    };
+  }
+})();
+
+
